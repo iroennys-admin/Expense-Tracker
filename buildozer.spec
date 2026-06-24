72eba5f5d1bd6dbcf655c8193d2f2308b29ffad7
iroennys-admin <iroennys-admin@example.com>
2026-06-24 18:59:47 -0400

    Add GitHub Actions APK build (32/64 bit) with Buildozer

---
+++ buildozer.spec
@@ -0,0 +1,69 @@
+[app]
+
+title = Expense Tracker
+package.name = expensetracker
+package.domain = org.iroennys
+
+source.dir = .
+source.include_exts = py,png,jpg,kv,atlas,html,css,txt,json,otf,ttf,md
+
+version = 1.0.0
+version.regex = __version__ = ['\"](.*)['\"]
+version.filename = %(source.dir)s/main.py
+
+requirements = python3, django, sqlite3, setuptools
+
+presplash.color = #1a1a2e
+
+orientation = portrait
+
+osx.package_name = Expense Tracker
+
+android.minapi = 21
+android.api = 34
+android.ndk = 25b
+
+android.archs = armeabi-v7a
+
+android.permissions = INTERNET
+
+android.add_activity = True
+android.wakelock = True
+
+android.accept_sdk_license = True
+
+android.gradle_dependencies = 'androidx.webkit:webkit:1.12.1'
+
+android.add_src =
+
+android.add_aars =
+
+android.add_jars =
+
+android.private_storage_path =
+
+android.allow_backup = True
+
+android.keystore =
+
+android.keystore.password =
+
+android.keyalias =
+
+android.keyalias.password =
+
+android.manifest.extra = <uses-feature android:glEsVersion="0x00020000" android:required="True" />
+
+android.strings =
+
+android.appclass =
+
+android.use_shlib = True
+
+android.copy_libs =
+
+[buildozer]
+
+log_level = 2
+
+warn_on_root = 0
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import threading
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestor_gastos.settings')
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+import django
+django.setup()
+
+from django.conf import settings
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+
+if 'ANDROID_PRIVATE' in os.environ:
+    settings.DATABASES['default']['NAME'] = os.path.join(os.environ['ANDROID_PRIVATE'], 'expenses.db')
+    settings.DEBUG = False
+    settings.ALLOWED_HOSTS = ['*']
+
+call_command('migrate', '--run-syncdb')
+
+application = get_wsgi_application()
+
+from wsgiref.simple_server import make_server
+
+server = make_server('127.0.0.1', 8520, application)
+threading.Thread(target=server.serve_forever, daemon=True).start()
+
+from jnius import autoclass
+
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+WebView = autoclass('android.webkit.WebView')
+LinearLayout = autoclass('android.widget.LinearLayout')
+LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
+
+activity = PythonActivity.mActivity
+
+webview = WebView(activity)
+webview.getSettings().setJavaScriptEnabled(True)
+webview.getSettings().setDomStorageEnabled(True)
+webview.loadUrl('http://127.0.0.1:8520')
+
+activity.setContentView(webview)
